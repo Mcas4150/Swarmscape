1ee31bc87ed56fc61a14b5109053ef98b67401ae
Michael Cassidy <mcassidy@sonobi.com>
2022-06-30 10:31:59 -0400

    add foods

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using extOSC;
 using UnityEngine;
@@ -13,74 +11,84 @@ public class Food : MonoBehaviour
     [Header("Spawn Setup")]
     [SerializeField] private FoodUnit foodUnit;
 
-
     public Queue<FoodUnit> foodAvailable = new Queue<FoodUnit>();
     public List<FoodUnit> Foods = new();
     public ObjectPool<FoodUnit> foodPool;
-    public OSCReceiver oscReceiver;
+    //public OSCReceiver oscReceiver;
 
     public int foodSize;
-    public int foodBounds = 75;
-    public int foodReceived = 50;
+    //public int foodBounds = 75;
+    //public int foodReceived = 50;
     public int foodCount = 0;
     public bool initialized = false;
-    //public int activeFood;
-    //public int inactiveFood;
 
-    public float scale(float OldValue, float OldMin, float OldMax, float NewMin, float NewMax)
-    {
+    //public float scale(float OldValue, float OldMin, float OldMax, float NewMin, float NewMax)
+    //{
 
-        float OldRange = (OldMax - OldMin);
-        float NewRange = (NewMax - NewMin);
-        float NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
+    //    float OldRange = (OldMax - OldMin);
+    //    float NewRange = (NewMax - NewMin);
+    //    float NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
 
-        return (NewValue);
-    }
+    //    return (NewValue);
+    //}
 
-    private void Awake()
-    {
-        oscReceiver.Bind("/flucoma/total", TotalFood);
-        oscReceiver.Bind("/flucoma/xyz", SeedFood);
-    }
-
-    private void Start()
-    {
-
-    }
-
-    private void TotalFood(OSCMessage message)
-    {
-        foodReceived = message.Values[0].IntValue;
-    }
+    //private void Awake()
+    //{
+    //    oscReceiver.Bind("/flucoma/total", TotalFood);
+    //    oscReceiver.Bind("/flucoma/xyz", SeedFood);
+    //}
 
 
-    protected void SeedFood(OSCMessage message)
-    {
-        var newNumber = message.Values[0].IntValue;
-        var newFoodX = scale(message.Values[1].FloatValue, 0f, 1f, -foodBounds, foodBounds);
-        var newFoodY = scale(message.Values[2].FloatValue, 0f, 1f, -foodBounds, foodBounds);
-        var newFoodZ = scale(message.Values[3].FloatValue, 0f, 1f, -foodBounds, foodBounds);
-        Vector3 newPosition = new Vector3(newFoodX, newFoodY, newFoodZ);
-
-        var newFood = InitializeFood(newPosition, newNumber);
-        foodCount++;
-        foodAvailable.Enqueue(newFood);
-
-        if (foodCount < foodSize) { EnableFoods(1); }
-    }
+    //private void TotalFood(OSCMessage message)
+    //{
+    //    foodReceived = message.Values[0].IntValue;
+    //}
 
 
-    public FoodUnit InitializeFood(Vector3 position, int index)
+    //protected void SeedFood(OSCMessage message)
+    //{
+    //    var newNumber = message.Values[0].IntValue;
+    //    var newFoodX = scale(message.Values[1].FloatValue, 0f, 1f, -foodBounds, foodBounds);
+    //    var newFoodY = scale(message.Values[2].FloatValue, 0f, 1f, -foodBounds, foodBounds);
+    //    var newFoodZ = scale(message.Values[3].FloatValue, 0f, 1f, -foodBounds, foodBounds);
+    //    Vector3 newPosition = new Vector3(newFoodX, newFoodY, newFoodZ);
+
+    //    var newFood = InitializeFood(newPosition, newNumber);
+    //   foodCount++;
+    //    foodAvailable.Enqueue(newFood);
+
+    //    if (foodCount < foodSize) { EnableFoods(1); } 
+    //}
+
+
+    //public FoodUnit InitializeFood(Vector3 position, int index)
+    //{
+
+    //    var food = Instantiate(foodUnit, position, Quaternion.identity, transform);
+    //    food.NumberIndex(index);
+    //    food.gameObject.SetActive(false);
+    //    food.AssignFood(this);
+
+    //    return food;
+
+    //}
+
+
+    public void InitializeFood(Vector3 position, int index)
     {
 
         var food = Instantiate(foodUnit, position, Quaternion.identity, transform);
         food.NumberIndex(index);
         food.gameObject.SetActive(false);
         food.AssignFood(this);
+        foodCount++;
+        foodAvailable.Enqueue(food);
 
-        return food;
+        if (foodCount < foodSize) { EnableFoods(1); }
+
 
     }
+
 
 
     public void EnableFoods(int number)
@@ -96,22 +104,6 @@ public class Food : MonoBehaviour
         }
 
     }
-
-    //public void OnFoodDeath(object sender, FoodDeathEventArgs e)
-    //{
-
-    //    Foods.Remove(e.FoodObject);
-
-
-    //    if (ecosystem.season == "spring" || ecosystem.season == "summer")
-    //    {
-    //        //var random = new System.Random();
-    //        //int index = random.Next(OscFood.Count);
-    //        //GenerateFood(OscFood[index]);
-    //    }
-    //}
-
-
 
 
 }
---
+++ Assets/Scripts/Foods.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using extOSC;
+
+public class Foods : MonoBehaviour
+{
+
+    [SerializeField] public Food Food1;
+    [SerializeField] public Food Food2;
+    [SerializeField] public Food Food3;
+    public List<Food> allFoods;
+
+    public OSCReceiver oscReceiver;
+    public int foodReceived = 50;
+    public int foodBounds = 75;
+
+    public float BorderBoundary = 100;
+    // Start is called before the first frame update
+
+    public float scale(float OldValue, float OldMin, float OldMax, float NewMin, float NewMax)
+    {
+
+        float OldRange = (OldMax - OldMin);
+        float NewRange = (NewMax - NewMin);
+        float NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
+
+        return (NewValue);
+    }
+
+    private void Awake()
+    {
+        oscReceiver.Bind("/flucoma/total", TotalFood);
+        oscReceiver.Bind("/flucoma/xyz", SeedFood);
+
+        allFoods = new List<Food> { Food1, Food2, Food3 };
+    }
+
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    private void TotalFood(OSCMessage message)
+    {
+        foodReceived = message.Values[0].IntValue;
+    }
+
+    protected void SeedFood(OSCMessage message)
+    {
+        var newNumber = message.Values[0].IntValue;
+        var newFoodX = scale(message.Values[1].FloatValue, 0f, 1f, -foodBounds, foodBounds);
+        var newFoodY = scale(message.Values[2].FloatValue, 0f, 1f, -foodBounds, foodBounds);
+        var newFoodZ = scale(message.Values[3].FloatValue, 0f, 1f, -foodBounds, foodBounds);
+        Vector3 newPosition = new Vector3(newFoodX, newFoodY, newFoodZ);
+
+
+        var FoodHeight = scale(message.Values[2].FloatValue, 0f, 1f, -BorderBoundary, BorderBoundary);
+
+        //int FoodTier;
+        //if (FoodHeight >= BorderBoundary * 0.33f) { FoodTier = 3; }
+        //if (FoodHeight < BorderBoundary * 0.33f && FoodHeight >= BorderBoundary * -0.33f) { FoodTier = 2; }
+        //if (FoodHeight < BorderBoundary * -0.33f) { FoodTier = 1; }
+
+
+        if (FoodHeight >= BorderBoundary * 0.33f) { Food3.InitializeFood(newPosition, newNumber); }
+        if (FoodHeight < BorderBoundary * 0.33f && FoodHeight >= BorderBoundary * -0.33f) { Food2.InitializeFood(newPosition, newNumber); }
+        if (FoodHeight < BorderBoundary * -0.33f) { Food1.InitializeFood(newPosition, newNumber); }
+
+
+
+
+
+        //var newFood = InitializeFood(newPosition, newNumber);
+        //foodCount++;
+        //foodAvailable.Enqueue(newFood);
+
+        //if (foodCount < foodSize) { EnableFoods(1); }
+    }
+}
